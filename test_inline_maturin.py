a40957c27b6c4f7dcc5e89d018eb530e84c16ab6
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 11:34:15 +0900

    Update test_inline_maturin.py

--- test_inline_maturin.py
+++ test_inline_maturin.py
@@ -3,7 +3,7 @@ import inline_maturin
 def test_build():
 
     # ビルド
-    inline_maturin.build_maturin_project('maturin_test/pymodtest')
+    inline_maturin.build_maturin_project('pymodtest')
 
     # 関数のチェック
     import pymodtest
