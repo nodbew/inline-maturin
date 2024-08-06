dd5a38660fc901ca4262dd32a07007fc336615df
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 11:18:05 +0900

    Create inline_maturin.py

---
+++ inline_maturin.py
@@ -0,0 +1,26 @@
+import subprocess
+from pathlib import Path
+
+def run(cmd: str, **kwargs) -> subprocess.CompletedProcess:
+    '''
+    シェルコマンドを実行する関数
+    '''
+    return subprocess.run(cmd, shell = True, check = True, **kwargs)
+
+def build_maturin_project(name: str) -> None:
+    '''
+    RustのクレートをPythonのモジュールとして読み込む関数
+    '''
+
+    # カレントディレクトリを指定された名前のディレクトリに変更
+    # maturin buildはソースディレクトリで実行しなければならないため
+    os.chdir('./' + str(name))
+
+    # ビルドとインストール
+    run("maturin build --verbose")
+    for p in Path("./target/wheels").iterdir():
+        run(f"pip install {p}")
+      
+    os.chdir('..')
+
+    return 
