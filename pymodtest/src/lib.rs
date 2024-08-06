9016dab5e5a7f027bab5785c522b6c52495a3ce3
nodbew <165175929+nodbew@users.noreply.github.com>
2024-08-06 11:40:59 +0900

    Update lib.rs

--- pymodtest/src/lib.rs
+++ pymodtest/src/lib.rs
@@ -2,13 +2,13 @@ use pyo3::prelude::*;
 
 #[pyfunction]
 fn add_two(num: i64) -> i64 {
-    /// 2を足す関数
+    // 2を足す関数
     num + 2
 }
 
 
 #[pymodule]
-fn pymodtest(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pymodtest(_py: Python, m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(add_two, m)?)?;
     Ok(())
 }
